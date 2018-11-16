009aefccf44f78e388973794a7ea0fb21f41cb9e
anuraagpolisetty <anuraag@uw.edu>
2018-11-15 22:58:17 -0800

    Updates

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# ps-7-anuraag-polisetty
-Problem set 7 website for my INFO 340: Client-Side Web Development class using React Router
---
+++ precache-manifest.678eb68e54a2c4408a1f599124436678.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a7a4df93947e7ea011a",
+    "url": "/anuraag/static/js/runtime~main.6a7a4df9.js"
+  },
+  {
+    "revision": "008e48f1a545c7425a2e",
+    "url": "/anuraag/static/js/main.008e48f1.chunk.js"
+  },
+  {
+    "revision": "f936a97560603577c667",
+    "url": "/anuraag/static/js/1.f936a975.chunk.js"
+  },
+  {
+    "revision": "008e48f1a545c7425a2e",
+    "url": "/anuraag/static/css/main.5867d57d.chunk.css"
+  },
+  {
+    "revision": "e75da24bf1dbcab471ac1af461bae2fd",
+    "url": "/anuraag/index.html"
+  }
+];
